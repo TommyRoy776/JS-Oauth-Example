1dec690a97ed0849d13ae6b0fc15c9cd2774fd7b
Roy776 <jj21861@gmail.com>
2021-04-30 20:38:54 -0400

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const url = require("url");
+const http = require("http");
+const https = require("https");
+
+const port = 3000;
+const server = http.createServer();
+server.on("request", request_handler);
+
+function request_handler(){
+
+}
+
+
+
+
+
+
+/*
+*　　　　　　　　┏┓　　　┏┓+ +
+*　　　　　　　┏┛┻━━━┛┻┓ + +
+*　　　　　　　┃　　　　　　　┃ 　
+*　　　　　　　┃　　　━　　　┃ ++ + + +
+*　　　　　　 ████━████ ┃+
+*　　　　　　　┃　　　　　　　┃ +
+*　　　　　　　┃　　　┻　　　┃
+*　　　　　　　┃　　　　　　　┃ + +
+*　　　　　　　┗━┓　　　┏━┛
+*　　　　　　　　　┃　　　┃　　　　　　　　　　　
+*　　　　　　　　　┃　　　┃ + + + +
+*　　　　　　　　　┃　　　┃　　　　Code is far away from bug with the animal protecting　　　　　　　
+*　　　　　　　　　┃　　　┃ + 　　　
+*　　　　　　　　　┃　　　┃
+*　　　　　　　　　┃　　　┃　　+　　　　　　　　　
+*　　　　　　　　　┃　 　　┗━━━┓ + +
+*　　　　　　　　　┃ 　　　　　　　┣┓
+*　　　　　　　　　┃ 　　　　　　　┏┛
+*　　　　　　　　　┗┓┓┏━┳┓┏┛ + + + +
+*　　　　　　　　　　┃┫┫　┃┫┫
+*　　　　　　　　　　┗┻┛　┗┻┛+ + + +
+*/
